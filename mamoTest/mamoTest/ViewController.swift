77b90a486b72d81d44b1a6040c4d6e769f985285
Roma Osiychuk <osiychukr@gmail.com>
2021-07-09 15:22:47 +0300

    project init git ignore

---
+++ mamoTest/mamoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mamoTest
+//
+//  Created by Roma Osiychuk on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
